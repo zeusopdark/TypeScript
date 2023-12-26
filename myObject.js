6d5ee62a24fefcc03d83bd908240adab05eb3dc7
zeusopdark <ankitnishad400@gmail.com>
2023-12-26 23:52:06 +0530

    type alias

--- myObject.js
+++ myObject.js
@@ -5,11 +5,16 @@ var User = {
     email: "ankit@gmail.com",
     isActive: true
 };
-function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
-}
-createUser({ name: "ankit", isPaid: true });
+// function createUser({ name: string, isPaid: boolean }) {
+// }
+// createUser({ name: "ankit", isPaid: true });
 function createCourse() {
     return { name: "ankit", isPaid: false };
 } //return type is an object {}
+//so the wierd part of passing object is that when we add extra parameter which is not defined in the function than it will give the error that's really weired but now the more wierd part is that when we define a variable and which is an object and that contains all the things which we defined in the function parameter and also additionaly we added some more variable and than pass that variable while calling the function than it will not give the error this is very wierd but this is how it works.
 createCourse();
+//now the type is User for the user which will have all the thing which we defined already above.
+function createUser(user) {
+    return { name: "", email: "", isActive: true };
+}
+createUser({ name: "", email: "", isActive: true });
--- myObject.ts
+++ myObject.ts
@@ -5,11 +5,11 @@ const User = {
 }
 
 
-function createUser({ name: string, isPaid: boolean }) {
+// function createUser({ name: string, isPaid: boolean }) {
 
-}
+// }
 
-createUser({ name: "ankit", isPaid: true });
+// createUser({ name: "ankit", isPaid: true });
 
 function createCourse(): { name: string, isPaid: boolean } {
     return { name: "ankit", isPaid: false }
@@ -18,5 +18,21 @@ function createCourse(): { name: string, isPaid: boolean } {
 //so the wierd part of passing object is that when we add extra parameter which is not defined in the function than it will give the error that's really weired but now the more wierd part is that when we define a variable and which is an object and that contains all the things which we defined in the function parameter and also additionaly we added some more variable and than pass that variable while calling the function than it will not give the error this is very wierd but this is how it works.
 
 createCourse();
+
+
+// type alias 
+
+type User = {
+    name: string,
+    email: string,
+    isActive: boolean
+}
+
+//now the type is User for the user which will have all the thing which we defined already above.
+function createUser(user: User): User {
+    return { name: "", email: "", isActive: true }
+}
+
+createUser({ name: "", email: "", isActive: true })
 
 export { };
