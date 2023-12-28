276998d2e609297b1b80faaa8ea03745af23a034
zeusopdark <ankitnishad400@gmail.com>
2023-12-28 21:48:41 +0530

    Enumerators

---
+++ myEnums.ts
@@ -0,0 +1,9 @@
+enum SeatChoice {
+    AISLE = "aisle",
+    MIDDLE = "middle",
+    WINDOW = "window",
+    FOURTH = 0
+}
+//values are provided by default its by default but we can also give our as well .
+// its up to use how we want if i put 3 in the middle than rest of them will have the next value 
+const hcSeat = SeatChoice.AISLE
