27226e886fc29fa01c1341612dbbd02eaf84f127
zeusopdark <ankitnishad400@gmail.com>
2023-12-28 22:01:18 +0530

    interface

---
+++ myInterface.ts
@@ -0,0 +1,12 @@
+interface User {
+    email: String,
+    userId: number,
+    googleId?: string,
+    startTrial(): string
+}
+
+const hitesh: User = {
+    email: "ankit@gmail.com",
+    userId: 22121,
+    startTrial: () => { return "" }
+}
